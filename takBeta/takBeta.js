6b72f606c8f3ed26a3f5c8cb620051984c02f61c
Paul Zavalza <paulz_91@hotmail.com>
2014-08-01 19:47:24 -0500

    with roles animation

--- takBeta/takBeta.js
+++ takBeta/takBeta.js
@@ -21,11 +21,21 @@ Router.map(function() {
   this.route('loginForm', {path: '/login'});
   this.route('newUserForm', {path: '/newUser'});
   this.route('news', {path: '/news'});
+  this.route('projects', {path: '/projects'});
 });
 
 
 if (Meteor.isClient) {
 
+  Meteor.startup(function () {
+    Session.set("management", false);
+    Session.set("development", false);
+    Session.set("design", false);
+    Session.set("investment", false);
+    Session.set("marketing", false);
+    Session.set("mentor", false);
+    Session.set("sales", false);
+  });
   Template.loginForm.events({
     'click .tryFacebookLogin': function(evt, tmpl){
       if(Accounts.loginServicesConfigured()){
@@ -67,6 +77,52 @@ if (Meteor.isClient) {
       }
     }
   });
+  Template.roles.events({
+  'click a' : function (evt, tmpl){
+      var sourceName = evt.target.src;
+      var variable = sourceName.slice(sourceName.lastIndexOf("/")+1, sourceName.lastIndexOf("."));
+      //alert(variable);
+       Session.set(variable, !Session.get(variable));
+    },     
+  });
+
+  Template.roles.management = function(){
+    return Session.get("management");
+  }
+
+  Template.roles.development= function(){
+    return Session.get("development");
+  }
+  Template.roles.design = function(){
+    return Session.get("design");
+  }
+  Template.roles.investment = function(){
+    return Session.get("investment");
+  }
+  Template.roles.marketing = function(){
+    return Session.get("marketing");
+  }
+  Template.roles.mentor = function(){
+    return Session.get("mentor");
+  }
+  Template.roles.sales = function(){
+    return Session.get("sales");
+  }
+
+  Template.navigation.events({
+  'click .tryLogout': function (evt, tmpl) {
+        Meteor.logout(function(err){
+            if (err)
+            {
+              //To do if logout was not successfull
+            }
+            else{
+                Router.go('loginForm');
+              }
+            });
+        return false
+      }
+  });
 }
 
 if (Meteor.isServer) {
@@ -78,6 +134,7 @@ if (Meteor.isServer) {
       firstName = user.services.facebook.first_name;
       lastName = user.services.facebook.last_name;
       fbLink = user.services.facebook.link;
+      fbPicture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
 
       //graph request for picture
     }
@@ -87,7 +144,7 @@ if (Meteor.isServer) {
     var profile ={
                       firstName:firstName,
                       lastName:lastName,
-                      picture:"", //url of picture in mongoDB
+                      picture:fbPicture, //url of picture
                       facebook_url:fbLink,
                       biography:"",
                       //college:"",
