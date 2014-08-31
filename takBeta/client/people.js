7452701d3745ef73b5d62f1abad53e3eecb7ac40
Paul Zavalza <paulz_91@hotmail.com>
2014-08-31 11:51:00 -0500

    update of peopople template using keyboard for filtering

--- takBeta/client/people.js
+++ takBeta/client/people.js
@@ -14,13 +14,16 @@ Template.people.events({
     Session.set('filters', filtersArray);
   },
 
-  'keyup #City,#Skill,#College,#Role,#Company' : function(evt, tmpl){
+  'keyup #City,#Skill,#Company' : function(evt, tmpl){
       //busca todo el string y no palabra por palabra
+      //alert(evt.keyCode);
+
       var targetId = evt.target.id;
       //alert(targetId)
       filter = tmpl.find('#'+targetId).value.trim().toUpperCase();
       var re = /([a-zA-Z]+)/g;
       var options = document.getElementsByClassName(targetId);
+      var matches = [];
       for (var i = 0; i < options.length; i++) {
 
         if (filter.match(re)){
@@ -30,6 +33,8 @@ Template.people.events({
               {
                 document.getElementById(targetId+'Options').style.display='inline';
                 options[i].style.display = 'list-item';
+                //options[i].className = targetId;
+                matches.push(options[i]);
               }
               
           else
@@ -42,22 +47,64 @@ Template.people.events({
         {
           document.getElementById(targetId+'Options').style.display='none';
           options[i].style.display = 'none';
-        }
-          
+        }   
       }
+
+      var key =evt.keyCode;
+      //alert(key);
+      var selection = Session.get('keyControl');
+
+      switch(key){
+        case 40: //Down
+          if(selection+1 < matches.length) 
+            selection = selection+1;
+        break;
+        case 38: //Up
+          if(selection-1 >= 0)
+          selection = selection -1;
+        break;
+        case 13: //Return
+          var filtersArray = Session.get("filters");
+          filtersArray.push(matches[selection].id);
+          Session.set('filters', filtersArray);
+          tmpl.find('#'+targetId).value = "";
+            tmpl.find('#'+targetId).blur();
+            document.getElementById(targetId+'Options').style.display='none';
+        break;
+        default:
+          selection = -1;
+        break;
+      }
+
+      for(var j=0; j < matches.length; j++)
+      {
+        if(j==selection)
+          matches[j].className =targetId+' list-group-item active';
+        else
+          matches[j].className = targetId+' list-group-item';
+      }
+      matches = [];
+      //Session.set('keyControl', 0);
+      Session.set('keyControl', selection);
+      //alert(selection);
     },
 
-    'click .City,.Skill,.College,.Role' : function (evt, tmpl){
-      //not sure what is better (more secure). To obtain id using target or this._id
-      //alert(evt.target.id);
+    'blur #City,#Skill' : function(evt, tmpl){
+      var targetId = evt.target.id;
+      //alert(evt.currentTarget.id);
+      Session.set('keyControl', -1);
+      tmpl.find('#'+targetId).value = "";
+      document.getElementById(targetId+'Options').style.display='none';
+      
+    },
+
+    'mousedown .City,.Skill' : function (evt, tmpl){
+      //alert(this._id);
       var targetClass = evt.target.getAttribute('class');
       var filtersArray = Session.get("filters");
       filtersArray.push(evt.target.id);
       Session.set('filters', filtersArray);
-      tmpl.find('#'+targetClass).value = "";
-      tmpl.find('#'+targetClass).blur();
-      document.getElementById(targetClass+'Options').style.display='none';
-      return true;
+      //blur event is called after mousedown
     },
 
     'click .pullFilter': function(evt, tmpl){
